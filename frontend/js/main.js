591f1904879037d3edb710fb92a183f646b87f06
adam-arold <arold.adam@gmail.com>
2017-07-17 21:44:25 +0200

    adding frontend project with kotlin2js configuration adding configuration to copy over generated js files to the main project updating the index template to use the copied js modules (requirejs)

---
+++ frontend/js/main.js
@@ -0,0 +1,9 @@
+requirejs.config({
+    paths: {
+        "kotlin": 'module/kotlin',
+        "todomvc": "module/todomvc"
+    }
+});
+
+requirejs(["todomvc"], function (todomvc) {
+});
---
+++ frontend/src/main/kotlin/todomvc/main.kt
@@ -0,0 +1,5 @@
+package todomvc
+
+fun main(args: Array<String>) {
+    println("Hello, from kotlin2js!")
+}
